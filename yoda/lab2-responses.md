95064799ba48a1aa8cbf2f3a14fbe5cf8b33de41
K Z <kz@dhcp-10-250-219-109.harvard.edu>
2020-10-23 21:00:08 -0400

    Submit Yoda lab.

--- yoda/lab2-responses.md
+++ yoda/lab2-responses.md
@@ -12,6 +12,6 @@ C. Welcome to Programming in Javascript
 ---------------------------------------
 3. 
     y.includes("hello") returns a true if "hello" is in y and false if not. In general, the includes functino returns whether or not its argument is contained in the string.  
-    y.repeat(10) prints out 10 consecutive copies of the string y. In general, the repeat function takes in argument x prints out x repetitions of the string.
+    y.repeat(10) prints out 10 consecutive copies of the string y. In general, the repeat function takes in argument x and prints out x repetitions of the string.
 4. 
     square(2.5) returns the square of 2.5, or 6.25. square("hello") returns NaN, which means "not a number," since "hello" is not a valid number that can be squared.
--- yoda/response_handler.js
+++ yoda/response_handler.js
@@ -19,7 +19,51 @@ let std_quotes = ["Patience you must have, my young padawan.",
 "Difficult to see. Always in motion is the future."
 ];
 
+let all_quotes = [dark_quotes, force_quotes, std_quotes];
+
+var inputElement = document.getElementById("input-text");
+inputElement.addEventListener("keyup", function(event) {
+    if (event.key == "Enter") {
+        respond();
+    }
+});
+
+
 function respond() {
     // Your Code Here
-    console.log("Hello World!");
+    var textElement = document.getElementById("yoda-text")
+    var imageElement = document.getElementById("yoda-image")
+    var inputElement = document.getElementById("input-text");
+
+    // console.log(inputElement);
+
+    var images = ["img/cute-std.jpg", "img/cute-force.jpg", "img/cute-dark.jpg", "img/regular-dark.jpg", "img/regular-force.jpg", "img/regular-std.jpg"];
+    var imageInd;
+    if (inputElement.value.includes("cute") || inputElement.value.includes("baby")) {
+        imageInd = parseInt(Math.random()*3);
+    }
+    else if (inputElement.value.includes("force")){
+        if (inputElement.value.includes("dark")) {
+            imageInd = parseInt(Math.random()*2 + 2);
+        }
+        else {
+            imageInd = parseInt(Math.random()*4 + 1);
+        }
+    }
+    else {
+        imageInd = parseInt(Math.random()*6);
+    }
+    imageElement.setAttribute("src", images[imageInd]);
+
+    textElement.innerHTML = generateQuote(imageInd);
+    document.getElementById("input-text").value = '';
+    // console.log("Hello World!");
+}
+
+function generateQuote(imageInd) {
+    if (imageInd > 2) {
+        return all_quotes[imageInd-3][parseInt(Math.random()*5)];
+    }
+    numMs = Math.random()*20 + 1;
+    return "H"+"m".repeat(numMs);
 }
